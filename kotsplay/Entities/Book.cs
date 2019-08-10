859eed2b8d3eb48fc739fa01a137ab5a393333c0
buglight <buglight@kistriver.com>
2019-08-10 20:16:01 +0300

    Book class

---
+++ kotsplay/Entities/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace kotsplay.Entities
+{
+    public class Book
+    {
+        public List<Recipe> Recipes { get; private set; }
+
+        public Book(string name, List<Ingridient> ingridients, List<Recipe> recipes)
+        {
+            Recipes = recipes;
+            foreach (var ingridient in ingridients)
+            {
+                if (!this.ingridients.ContainsKey(ingridient.Id))
+                    this.ingridients.Add(ingridient.Id, ingridient);
+            }
+            foreach (var recipe in recipes)
+            {
+                if (!this.ingridients.ContainsKey(recipe.Id))
+                    this.ingridients.Add(recipe.Id, recipe);
+            }
+        }
+
+        public BaseIngridient GetIngridient(int id)
+        {
+            return ingridients.GetValueOrDefault(id);
+        }
+
+        private readonly Dictionary<int, BaseIngridient> ingridients = new Dictionary<int, BaseIngridient>();
+    }
+}
